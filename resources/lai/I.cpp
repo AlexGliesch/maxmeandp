50121bfeb42e2945acdc4f8b547e52b6e6d5a95e
AlexGliesch <alexgliesch@gmail.com>
2022-05-11 04:19:00 -0300

    Some test results. Add script to generate large instances.

---
+++ resources/lai/I.cpp
@@ -0,0 +1,84 @@
+  
+/*****************************************************************************\
+ * This program aims to generate the benchmark instances used in our paper   *
+ * for the weighted Max-Mean dispersion problem.                             * 
+\*****************************************************************************/ 
+                                                                                                                                         
+/*****************************************************************************/
+/**********          0. Header files and variables      **********************/
+/*****************************************************************************/ 
+#include <stdio.h>
+#include <stdlib.h>
+#include <iostream>
+#include <sstream>
+#include <fstream>
+#include <string.h>
+#include <time.h>
+#include <ctime>
+#include <vector>
+#include <math.h>
+// #include <conio.h>
+#include <ctype.h>
+using namespace std;
+FILE * fp ;
+int N;        // node number in graph
+double  ** D; // distance matrix
+double  * w ; // node weight 
+const double MW = 4.0; 
+/*****************************************************************************/
+/*****************          1. Outputing  results      ***********************/
+/*****************************************************************************/ 
+void Outputing(int tt)
+{
+    int i,j;
+	FILE *fp; 
+	char buff[80];
+	
+    sprintf(buff,"%s_%d_%d.txt","I",N,tt);
+    fp=fopen(buff,"a+");
+    fprintf(fp,"%d\n", N); 
+    for(i=0;i<N;i++)  fprintf(fp,"%d   %5.2lf\n",i+1, w[i]); 
+    for(i=0;i<N;i++)
+    for(j=i+1;j<N;j++) 
+      fprintf(fp,"%d   %d   %5.2lf\n",i+1, j+1, D[i][j]); 
+	fclose(fp);
+}
+
+/*****************************************************************************/
+/*****************         2. Main Function           ************************/
+/*****************************************************************************/ 
+int main(int argc, char **argv)
+{ 
+     int i, j, i1, j1, seed ; 
+     int tt;
+
+     N = atoi(argv[1]);
+    
+     seed = N; //seed= 1000 for N = 1000, seed= 3000 for N =3000, seed= 5000 for N = 5000
+     srand( seed ) ;
+  
+     int sign;
+    //  N = 5000;    //N = 1000, 3000, 5000. 
+     w = new double [N]; 
+     D = new double *[N];
+     for(i=0;i<N;i++)
+     D[i] = new double [N];
+     for(tt=1;tt<=10;tt++)
+     {
+      for(i=0;i<N;i++)
+      for(j=i+1;j<N;j++)
+      {  
+         if(rand()%2) sign = 1;
+         else sign = -1; 
+         D[i][j] = sign*((rand()%30000)/30000.0)*10.0 ; 
+         D[j][i] = D[i][j]; 
+      } 
+      
+      for(i=0;i<N;i++) w[i] = 1.0 + MW *((rand()%30000)/30000.0); 
+      
+      Outputing(tt);
+    }
+
+  return 0;
+}
+ 
---
+++ test/README.md
@@ -0,0 +1,13 @@
+#   | Date       | Status  | Instance set   | Seeds | TL(s) | Description
+--- | ---------- | ------- | -------------- | ----- | ----- | --------------------------------------------------
+1   | 2022-04-29 | ok      | brimberg       | 1-10  | 600   | Testing Lai's code
+2   | 2022-04-29 | ok      | brimberg-large |       | 300   | First irace calibration
+3   | 2022-05-11 | pending | brimberg       | 1-10  | 600   | Testing our calibrated code
+4   | 2022-05-11 | pending | lai            | 1-10  | 600   | Lai's code on large instances
+5   | 2022-05-11 | pending | lai            |       | 600   | Recalibrating our code with irace on large instances
+
+## Changelog
+
+2022-04-29: instance set 'brimberg' has all instances from Brimberg et al. (2017)
+2022-04-29: instance set 'brimberg-large' has instances of size [1000,1500,2000] from Brimberg et al. (2017)
+2022-05-11: instance set 'lai' has all instances from Lai et al. (2020)
